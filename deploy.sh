ae28bb5002a1ffbe819b55db0c7ec86b464a67e5
Daniel Alarms <danielalarmz@gmail.com>
2019-10-19 00:43:39 +0100

    Modify output of deployment script for clarity

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+echo -e "\e[32m===============  Merging dev branch into master  ===============\n\e[39m";
 git checkout master;
-echo 'Merging dev branch into master';
 git merge dev;
-echo 'Done merging into master';
-echo 'Pushing to origin';
+echo -e "\e[36m\n>>>>>>>>>>>>>> Done merging into master    <<<<<<<<<<<<<<<<<<<<";
+echo -e "\e[32m\n===============    Pushing to origin   ===============\n\e[39m";
 git commit;
 git push;
-echo 'Done pushing to origin';
-echo 'Building production app on server';
-ssh -i Instance1.pem ec2-user@dlarm.me "cd /var/www/itsdlarm; sudo ./update; exit;";
+echo -e "\e[36m\n>>>>>>>>>>>>>> Done pushing into origin    <<<<<<<<<<<<<<<<<<<<";
+echo -e "\e[32m\n===============    Building on production server   ===============\n\e[39m";
+ssh -i ~/Instance1.pem ec2-user@dlarm.me "cd /var/www/itsdlarm; sudo ./update; exit;";
+git checkout dev;
+echo -e "\e[36m\n>>>>>>>>>>>>>> Deployment Complete <<<<<<<<<<<<<<<<<<<<\e[39m";
 
 
