66a3660913314e8d5e2c930ed9c2e9b257a72479
Daniel Alarms <danielalarmz@gmail.com>
2019-10-19 00:18:25 +0100

    Remove client from pm2 config

--- itsdlarm.config.js
+++ itsdlarm.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   apps : [
     {
-      name        : "itsdlarm-client",
-      script      : "npm",
-      args	      : "run start-client",
-      watch       : true,
-      env         : {
-        "NODE_ENV": "development",
-      },
-      env_production : {
-      "NODE_ENV": "production"
-      }
-    },
-    {
       name        : "itsdlarm-server",
       script      : "npm",
       args	      : "run start-server",
