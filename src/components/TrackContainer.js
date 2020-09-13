6408c875e42664c03f2d7069fd042ebd2e10062c
Daniel Alarms <danielalarmz@gmail.com>
2020-09-13 02:08:42 +0100

    Styling changes to title and container

--- src/components/TrackContainer.js
+++ src/components/TrackContainer.js
@@ -5,9 +5,9 @@ const TrackContainer = ( { title, children } ) => {
   return (
     <div className="track-container">
       {/* <div className="container-title">{title}</div> */}
-      <i className="fas fa-chevron-circle-left track-container-left"></i>
+      {/* <i className="fas fa-chevron-circle-left track-container-left"></i> */}
       {children}
-      <i className="fas fa-chevron-circle-right track-container-right"></i>
+      {/* <i className="fas fa-chevron-circle-right track-container-right"></i> */}
     </div>
   );
 }
